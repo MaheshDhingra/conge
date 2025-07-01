21a56df34bfa9ef4144240397d5295be97d06742
Maheshishere <108296592+MaheshTheDeveloper@users.noreply.github.com>
2025-07-01 10:16:08 +0530

    v0.0.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
 export default function Home() {
   return (
-    <p>Hello user!</p>
+    <>
+      <h1>My Games</h1>
+      <ul>
+        <li>
+          <Link href="/game1">
+            Game 1
+          </Link>
+        </li>
+        <li>
+          <Link href="/game2">
+            Game 2
+          </Link>
+        </li>
+        <li>
+          <Link href="/game3">
+            Game 3
+          </Link>
+        </li>
+      </ul>
+    </>
   );
 }
